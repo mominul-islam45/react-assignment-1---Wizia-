fae8834e926347c1e34ab136327b413511d53043
mominul-islam45 <mominul4586@gmail.com>
2025-01-22 19:29:58 +0600

    partners section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './Components/Hero'
 import NavArea from './Components/NavArea'
+import Partners from './Components/Partners'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <NavArea/>
       <Hero/>
+      <Partners/>
     </>
   )
 }
